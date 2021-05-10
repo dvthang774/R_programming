8973c2d58e7004a30f585d594f23921e9397a0af
dvthang774 <74769024+dvthang774@users.noreply.github.com>
2021-05-10 07:43:21 +0700

    R

---
+++ R_dev.R
@@ -0,0 +1,33 @@
+setwd('C:\\Users\\Admin')
+Owls = read.table(file = "Owls.txt", header = TRUE)
+names(Owls)
+str(Owls)
+unique(Owls$Nest)
+
+Owls.ATV = Owls[Owls$Nest == "AutavauxTV",]
+plot(x = Owls.ATV$ArrivalTime,
+     y = Owls.ATV$NegPerChick,
+     xlab = "Arrival Time",
+     main = "AutavauxTV",
+     ylab = "Negotiation behaviour")
+Owls.Bot = Owls[Owls$Nest=="Bochet",]
+plot(x = Owls.ATV$ArrivalTime,
+     y = Owls.ATV$NegPerChick,
+     xlab = "Arrival Time",
+     main = "Bochet",
+     ylab = "Negotiation behaviour")
+
+Allnests = unique(Owls$Nest)
+for (i in 1:27){
+  Nest.i = Allnests[i]
+  Owls.i = Owls[Owls$Nest == Nest.i,]
+  YourFileName = paste(Nest.i,".jpg",sep = "")
+  jpeg(file = YourFileName)
+  plot(x = Owls.i$ArrivalTime, y = Owls.i$NegPerChick,
+       xlab = "Arrival Time",
+       main = Nest.i,
+       ylab = "Negotiation behaviour"
+  )
+  dev.off()
+}
+
